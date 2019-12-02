af9598bf4c4afe134c8233074dbd4b7b9d4bacf8
razantayyem <razanr543@gmail.com>
2019-12-02 23:16:20 +0200

    update sequlize.js2

--- server/database/models/sequelize.js
+++ server/database/models/sequelize.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('env2')('config.env');
 
 // init sequelize
-const sequelize = new Sequelize(process.env.DB_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false,
   dialect: 'postgres',
   dialectOptions: {

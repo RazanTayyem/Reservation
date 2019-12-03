d6cd677a73948ec004c25e1522080688095d7449
ameentalahmeh <ayman201266@hotmail.com>
2019-12-03 09:30:06 +0200

    update app.js2

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use(router);
 
-app.get('/', (_req, res) => {
+app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
